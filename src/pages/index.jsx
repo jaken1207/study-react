041f8734f8b2a456ec721a02c02ba4ecd44ee2f5
naga <yuto7687@gmail.com>
2022-12-20 22:35:49 +0900

    ✨ useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer"
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 
 
@@ -17,6 +17,16 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   },[]);
+
+  useEffect(() =>{
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("アンマウント時");
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
