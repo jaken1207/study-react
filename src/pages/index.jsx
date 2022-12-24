8434148b018eabe038e82d8bd22d5736c695f2d1
naga <yuto7687@gmail.com>
2022-12-25 04:09:05 +0900

    🎨 Custom Hooksを使ってまとめた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,51 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer"
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect, useState } from 'react'
+import { useBgLightBlue } from 'src/hooks/useBgLightBlue'
+import { useInputArray } from 'src/hooks/useInputArray'
+import { useCounter } from 'src/hooks/useCounter'
+
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow)=> !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) =>{
-    if(e.target.value.length > 5) {
-      alert("5文字以上入力できません");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)) {
-        alert("同じ要素がすでに存在する");
-        return prevArray;
-      } 
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() =>{
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -57,6 +23,7 @@ export default function Home() {
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange}/>
       <button onClick={handleAdd}>追加</button>
       <ul>
@@ -66,6 +33,7 @@ export default function Home() {
           );
         })}
       </ul>
+
       <Main page="index" />
       <Footer />
     </div>
